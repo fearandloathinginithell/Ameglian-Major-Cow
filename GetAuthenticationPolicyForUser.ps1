e624ddd25fc4e914407e1715f9978a7101a2e5b2
fearandloathinginithell <53252594+fearandloathinginithell@users.noreply.github.com>
2019-11-27 14:32:48 +1300

    Update GetAuthenticationPolicyForUser.ps1

--- GetAuthenticationPolicyForUser.ps1
+++ GetAuthenticationPolicyForUser.ps1
@@ -2,11 +2,15 @@ $user= "scanner"
 if((Get-User -Identity $user | select -ExpandProperty AuthenticationPolicy) -eq $null) {
    Write-Host "User"
    Get-User -Identity $user | select DisplayName,Identity,WindowsEmailAddress,UserPrincipalName |fl
-   Write-Host "Effective Authentication Policy" 
-   Get-AuthenticationPolicy -identity (Get-OrganizationConfig | select -ExpandProperty DefaultAuthenticationPolicy) | select Name,Allow* |fl
+   Write-Host "Effective Authentication Policy from the default assignment"
+        if((Get-OrganizationConfig | select -ExpandProperty DefaultAuthenticationPolicy) -eq $null) {
+        Write-Host "No Policy assigned"
+        }else {
+        Get-AuthenticationPolicy -identity (Get-OrganizationConfig | select -ExpandProperty DefaultAuthenticationPolicy) | select Name,Allow* |fl
+        }
 }else {
    Write-Host "User"
    Get-User -Identity $user | select DisplayName,Identity,WindowsEmailAddress,UserPrincipalName |fl
-   Write-Host "Effective Authentication Policy" 
+   Write-Host "Effective Authentication Policy from the direct assignment" 
    Get-AuthenticationPolicy -identity (Get-User -Identity $user | select -ExpandProperty AuthenticationPolicy) | select Name,Allow* |fl
 }
